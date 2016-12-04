75550998a37101b117d0bee01f12e86f7cae33d9
Daniel Richards <daniel.p.richards@gmail.com>
2016-12-04 16:44:19 +0000

    Make declare throw an error when a function is not specified for the first argument

--- src/declare.lua
+++ src/declare.lua
@@ -3,6 +3,9 @@ local function validateTypes(propTypes, props)
 end
 
 local function declare(renderFunc, propTypes)
+  assert(type(renderFunc) == 'function',
+    'expected first argument to declare to be a function')
+
   return function(props, children, key)
     validateTypes(propTypes, props)
     return renderFunc(props, children, key)
