5aad61909d1366001135246d1e27cc0f1b973a22
Daniel Richards <daniel.p.richards@gmail.com>
2016-12-10 13:23:45 +0000

    update test suite

--- tests/suite.lua
+++ tests/suite.lua
@@ -1,5 +1,9 @@
 return {
   'tests/specs/declare.lua',
   'tests/specs/propTypes.lua',
-  'tests/specs/validators/string.lua'
+  'tests/specs/validators/string.lua',
+  'tests/specs/validators/number.lua',
+  'tests/specs/validators/boolean.lua',
+  'tests/specs/validators/table.lua',
+  'tests/specs/validators/function.lua',
 }
