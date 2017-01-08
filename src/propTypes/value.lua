dcaa3578902fb5e7eb17447afffb50e143cd8344
Daniel Richards <daniel.p.richards@gmail.com>
2017-01-08 16:59:51 +0000

    Use extracted getPrintableValue helper instead of inline one

--- src/propTypes/value.lua
+++ src/propTypes/value.lua
@@ -1,20 +1,5 @@
-local function getPrintableValue(value)
-  if value == nil then
-    return 'nil'
-  elseif type(value) == 'function' then
-    return 'a function'
-  elseif value == true then
-    return 'true'
-  elseif value == false then
-    return 'false'
-  elseif type(value) == 'function' then
-    return 'a function'
-  elseif type(value) == 'table' then
-    return 'a table'
-  else
-    return value
-  end
-end
+local path = (...):gsub("%.propTypes.value$","")
+local getPrintableValue = require(path .. '.helpers.getPrintableValue')
 
 local function value(description)
   assert(description ~= nil,
